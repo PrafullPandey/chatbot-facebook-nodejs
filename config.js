b029172adfd01d6e606bd7f7d524bb8fed7068fb
PrafullPandey <p2.prafull@gmail.com>
2017-08-30 17:14:26 +0530

    initial configuration

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    FB_PAGE_TOKEN: '',
-    FB_VERIFY_TOKEN: '',
-    API_AI_CLIENT_ACCESS_TOKEN: '',
-    FB_APP_SECRET: '',
-    SERVER_URL: "",
+    FB_PAGE_TOKEN: 'EAAbIfGSWGC8BAAboZBiIsw9iGlY2Rbt4ZAQX85EKtfKq3LU52EEEvQwWpXwWmkIZBgYuEsTcbkGbM7ZBQvvUcm1YbuMrq1riUaoAyEIMvxdN6sMvpe4gxwWW1eUDUevgIU3MRpJDCWadLCaWJcsxoQJkJB8dAA6eT7p1265R4QUKZC8SMURyO',
+    FB_VERIFY_TOKEN: 'Prafull123@',
+    API_AI_CLIENT_ACCESS_TOKEN: 'a855f47c4dad48889a6f635b59562dac',
+    FB_APP_SECRET: 'ada8c64320399e700496db4832c22b72',
+    SERVER_URL: " https://baby-jarvis.herokuapp.com/",
 };
